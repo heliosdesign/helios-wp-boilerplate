09afa50f170326aa7d88930d99096c97dbbe4bd1
dansundy <dansundy@gmail.com>
2013-10-02 11:14:57 -0400

    Initial adding of theme files

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Function file declearation.
+ *
+ */
+
+require_once( get_template_directory() . '/inc/functions/base-functions.php' );
+require_once( get_template_directory() . '/inc/functions/scripts.php' );
+
+?>
