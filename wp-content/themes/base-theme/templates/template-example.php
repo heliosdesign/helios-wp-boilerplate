dad47f3e5ee4a1e6e0f179b5c529853254a85b1e
dansundy <dansundy@gmail.com>
2017-01-10 14:14:33 -0500

    Add base theme example template.

---
+++ wp-content/themes/base-theme/templates/template-example.php
@@ -0,0 +1,29 @@
+<?php 
+/*
+ * Template Name: Example Page
+ * Description: An example page template.
+ */
+?>
+
+<?php get_header(); ?>
+
+  <?php if ( have_posts() ) : ?>
+
+    <?php helios_content_nav( 'nav-above' ); ?>
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+      <?php get_template_part( 'content/content', get_post_format() ); ?>
+
+    <?php endwhile; ?>
+
+    <?php helios_content_nav( 'nav-below' ); ?>
+
+  <?php else : ?>
+
+    <?php get_template_part( 'content/no-results' ); ?>
+
+  <?php endif; ?>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
